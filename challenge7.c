a2398969f5092f3171eae21f2011a0ddd8ca8599
mouad ziyani <ziyanimouad@gmail.com>
2025-09-09 21:46:22 +0100

    challenge7

---
+++ challenge7.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+
+int main() {
+    char str[100];
+    int i;
+    
+    printf("Entrer une chaine de caracteres : ");
+    fgets(str, sizeof(str), stdin);
+    str[strcspn(str, "\n")] = '\0'; 
+
+    for (i = 0; str[i] != '\0'; i++) {
+        str[i] = tolower(str[i]);
+    }
+
+    printf("Chaine en minuscules : %s\n", str);
+
+    return 0;
+}
+
