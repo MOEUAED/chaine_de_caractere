ac6825a8437f32e9db5d5d01843c623fb12f9b8b
mouad ziyani <156471093+MOEUAED@users.noreply.github.com>
2025-09-10 16:45:31 +0100

    Update challenge8.c

--- challenge8.c
+++ challenge8.c
@@ -1,24 +1,20 @@
 #include <stdio.h>
+#include <ctype.h>
 #include <string.h>
 
 int main() {
     char str[100];
-    int i, j = 0;
-
+    int i;
+    
     printf("Entrer une chaine de caracteres : ");
     fgets(str, sizeof(str), stdin);
     str[strcspn(str, "\n")] = '\0'; 
-    
-    for (i = 0; str[i] != '\0'; i++) {
-        if (str[i] != ' ') {
-            str[j] = str[i];
-            j++;
-        }
-    }
-    str[j] = '\0';
 
-    printf("Chaine sans espaces : %s\n", str);
+    for (i = 0; str[i] != '\0'; i++) {
+        str[i] = tolower(str[i]);
+    }
+
+    printf("Chaine en minuscules : %s\n", str);
 
     return 0;
 }
-
