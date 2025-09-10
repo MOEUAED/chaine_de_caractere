8eb232db094bc530b3e8a76fedf036dcb0a0f68a
mouad ziyani <156471093+MOEUAED@users.noreply.github.com>
2025-09-10 16:44:41 +0100

    Update challenge9.c

--- challenge9.c
+++ challenge9.c
@@ -1,25 +1,23 @@
-#include<stdio.h>
-#include<string.h>
+#include <stdio.h>
+#include <string.h>
 
-int main (){
-	
-	char str[50] ,str_es[50];
-	int i , j =0;
-	printf("entrer une chaie de caractere : ");
-	
-	fgets(str, sizeof(str), stdin);
-	
-	str[strcspn(str , "\n")]= '\0';
-	for (i=0;i<strlen(str);i++){
-		
-		if(str[i]!= ' '){
-			str_es[j]=str[i];
-			j++;
-		}
-		
-	}
-	
-	printf("votre de chaine de caractere est  %s",str_es);
-	
-	return 0;
+int main() {
+    char str[100];
+    int i, j = 0;
+
+    printf("Entrer une chaine de caracteres : ");
+    fgets(str, sizeof(str), stdin);
+    str[strcspn(str, "\n")] = '\0'; 
+    
+    for (i = 0; str[i] != '\0'; i++) {
+        if (str[i] != ' ') {
+            str[j] = str[i];
+            j++;
+        }
+    }
+    str[j] = '\0';
+
+    printf("Chaine sans espaces : %s\n", str);
+
+    return 0;
 }
